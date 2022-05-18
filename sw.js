d139a0a97a365e848429d71cabe6c2880a5cb810
bakzhol625 <80092987+bakzhol625@users.noreply.github.com>
2022-05-18 22:29:43 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,1 @@
-<script src="js/addtohomescreen.js"></script>
-    <script>
-        addToHomescreen( {
-            appID: "io.github.00fit",
-            appName: "00fit",
-            lifespan: 15,
-            autostart: true,
-            skipFirstVisit: true,
-            minSessions: 1,
-            displayPace: 0,
-            customPrompt: {
-                title: "Install 00fit?",
-                src: "meta/favicon-96x96.png",
-                cancelMsg: "Cancel",
-                installMsg: "Install"
-            }
-        } );
-    </script>
+
